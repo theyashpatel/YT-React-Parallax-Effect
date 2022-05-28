52bcce9188c55f196169e6e34fd3caee06919645
theyashpatel <yashpatel26@gmail.com>
2022-05-28 13:19:12 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Parallax } from "react-parallax";
+import Woman from "./images/woman.jpg";
+import City from "./images/city.jpg";
+import Fly from "./images/fly.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={300} bgImage={Woman}>
+        <div className="content">
+          <div className="text-content">Normal Parallax</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={300} blur={{ min: -5, max: 15 }} bgImage={City}>
+        <div className="content">
+          <div className="text-content">Blur</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={-600} bgImage={Fly}>
+        <div className="content">
+          <div className="text-content">Reverse Parallax</div>
+        </div>
+      </Parallax>
+
+      <div className="content"></div>
     </div>
   );
 }
